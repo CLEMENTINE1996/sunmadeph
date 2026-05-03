b2bf06a57983c8439fce457acc95af5f3945783b
Jason Aballe <jasonaballe0@gmail.com>
2026-05-03 13:49:18 +0800

    new update

--- src/components/Home/FeaturedPosts.js
+++ src/components/Home/FeaturedPosts.js
@@ -8,7 +8,7 @@ const FeaturedPosts = ({ blogs }) => {
   const sortedBlogs = sortBlogs(blogs);
   
   return (
-    <Slide duration={500} fraction={0.5} triggerOnce >
+    <Slide triggerOnce >
       <section className="w-full mt-16 sm:mt-24 md:mt-32 px-5 sm:px-10 md:px-24 sxl:px-32 flex flex-col items-center justify-center">
         <h2 className="w-full inline-block font-bold capitalize text-2xl md:text-4xl text-dark dark:text-light">
           Featured Posts
