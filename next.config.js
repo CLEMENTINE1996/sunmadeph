f46fb422a3dc2c46d5f9e9a294b10627a2d7f8a7
Jason Aballe <jasonaballe0@gmail.com>
2026-05-03 12:19:55 +0800

    new updates

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
   compiler: {
     removeConsole: process.env.NODE_ENV === "production",
   },
   output: 'export', 
   basePath: '/sunmadeph', 
+  assetPrefix: '/sunmadeph/',
   images: {
     unoptimized: true,
   },
 };
+
+module.exports = nextConfig;
